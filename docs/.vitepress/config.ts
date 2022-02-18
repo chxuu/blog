0928ac72efdc2b0e6c1d6737a0541503445b83d5
徐春红 <xuchunhong@xuchunhongdeMacBook-Pro.local>
2022-02-18 16:31:27 +0800

    init blog

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,11 @@
+/*
+ * @Date: 2022-01-30 11:06:27
+ * @LastEditors: chxu
+ * @LastEditTime: 2022-01-30 11:06:56
+ * @FilePath: /chxu-blog/docs/.vitepress/config.ts
+ * @Author: chxu
+ */
+module.exports = {
+  title: 'Hello VitePress',
+  description: 'Just playing around.'
+}
---
+++ docs/.vitepress/index.md
@@ -0,0 +1,8 @@
+<!--
+ * @Date: 2022-01-30 11:06:38
+ * @LastEditors: chxu
+ * @LastEditTime: 2022-01-30 11:08:46
+ * @FilePath: /chxu-blog/docs/.vitepress/index.md
+ * @Author: chxu
+-->
+# Hello VitePress
